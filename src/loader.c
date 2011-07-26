dd2f21f6a4b3c8e17ea24743d7d959030fc4f2fb
Jon <jon.forums@gmail.com>
2011-07-26 16:36:54 -0400

    Add skeleton for win/nix library loader

---
+++ src/loader.c
@@ -0,0 +1,8 @@
+#include "loader.h"
+
+HANDLE xvalid_lib_loader(char *filename)
+{
+	HANDLE dll = NULL;
+
+	return dll;
+}
---
+++ src/loader.h
@@ -0,0 +1,19 @@
+#ifndef LOADER_H
+#define LOADER_H
+
+#ifndef _WIN32
+#  include <dlfcn.h>
+#  define HANDLE void*
+#  define load_lib(i) dlopen((i), RTLD_LAZY | RTLD_GLOBAL)
+#  define symbol_from_lib dlsym
+#  define close_lib dlclose
+#else
+#  include <windows.h>
+#  define load_lib xvalid_lib_loader
+#  define symbol_from_lib GetProcAddress
+#  define close_lib FreeLibrary
+#endif /* _WIN32 */
+
+HANDLE xvalid_lib_loader(char *filename);
+
+#endif /* LOADER_H */
